6d930890c04a81234312628a243d54e248a5b476
Dev4Talal <77576574+Dev4Talal@users.noreply.github.com>
2021-04-24 21:30:28 +0800

    added magic auth

--- examples/web-push/pages/index.js
+++ examples/web-push/pages/index.js
@@ -1,94 +1,161 @@
-import { useEffect, useState } from 'react'
-import Head from 'next/head'
+import { useEffect, useState } from "react";
+import Head from "next/head";
 
-const base64ToUint8Array = base64 => {
-  const padding = '='.repeat((4 - (base64.length % 4)) % 4)
-  const b64 = (base64 + padding).replace(/-/g, '+').replace(/_/g, '/')
+import { useUser } from "../lib/hooks";
+import Layout from "../components/layout";
 
-  const rawData = window.atob(b64)
-  const outputArray = new Uint8Array(rawData.length)
+const base64ToUint8Array = (base64) => {
+  const padding = "=".repeat((4 - (base64.length % 4)) % 4);
+  const b64 = (base64 + padding).replace(/-/g, "+").replace(/_/g, "/");
+
+  const rawData = window.atob(b64);
+  const outputArray = new Uint8Array(rawData.length);
 
   for (let i = 0; i < rawData.length; ++i) {
-    outputArray[i] = rawData.charCodeAt(i)
+    outputArray[i] = rawData.charCodeAt(i);
   }
-  return outputArray
-}
+  return outputArray;
+};
 
 export default () => {
-  const [isSubscribed, setIsSubscribed] = useState(false)
-  const [subscription, setSubscription] = useState(null)
-  const [registration, setRegistration] = useState(null)
+  const [isSubscribed, setIsSubscribed] = useState(false);
+  const [subscription, setSubscription] = useState(null);
+  const [registration, setRegistration] = useState(null);
+
+  const user = useUser();
 
   useEffect(() => {
-    if (typeof window !== 'undefined' && 'serviceWorker' in navigator && window.workbox !== undefined) {
+    if (
+      typeof window !== "undefined" &&
+      "serviceWorker" in navigator &&
+      window.workbox !== undefined
+    ) {
       // run only in browser
-      navigator.serviceWorker.ready.then(reg => {
-        reg.pushManager.getSubscription().then(sub => {
-          if (sub && !(sub.expirationTime && Date.now() > sub.expirationTime - 5 * 60 * 1000)) {
-            setSubscription(sub)
-            setIsSubscribed(true)
+      navigator.serviceWorker.ready.then((reg) => {
+        reg.pushManager.getSubscription().then((sub) => {
+          if (
+            sub &&
+            !(
+              sub.expirationTime &&
+              Date.now() > sub.expirationTime - 5 * 60 * 1000
+            )
+          ) {
+            setSubscription(sub);
+            setIsSubscribed(true);
           }
-        })
-        setRegistration(reg)
-      })
+        });
+        setRegistration(reg);
+      });
     }
-  }, [])
+  }, []);
 
-  const subscribeButtonOnClick = async event => {
-    event.preventDefault()
+  const subscribeButtonOnClick = async (event) => {
+    event.preventDefault();
     const sub = await registration.pushManager.subscribe({
       userVisibleOnly: true,
-      applicationServerKey: base64ToUint8Array(process.env.NEXT_PUBLIC_WEB_PUSH_PUBLIC_KEY)
-    })
+      applicationServerKey: base64ToUint8Array(
+        process.env.NEXT_PUBLIC_WEB_PUSH_PUBLIC_KEY
+      ),
+    });
     // TODO: you should call your API to save subscription data on server in order to send web push notification from server
-    setSubscription(sub)
-    setIsSubscribed(true)
-    console.log('web push subscribed!')
-    console.log(sub)
-  }
+    setSubscription(sub);
+    setIsSubscribed(true);
+    console.log("web push subscribed!");
+    console.log(sub);
+  };
 
-  const unsubscribeButtonOnClick = async event => {
-    event.preventDefault()
-    await subscription.unsubscribe()
+  const unsubscribeButtonOnClick = async (event) => {
+    event.preventDefault();
+    await subscription.unsubscribe();
     // TODO: you should call your API to delete or invalidate subscription data on server
-    setSubscription(null)
-    setIsSubscribed(false)
-    console.log('web push unsubscribed!')
-  }
+    setSubscription(null);
+    setIsSubscribed(false);
+    console.log("web push unsubscribed!");
+  };
 
-  const sendNotificationButtonOnClick = async event => {
-    event.preventDefault()
+  const sendNotificationButtonOnClick = async (event) => {
+    event.preventDefault();
     if (subscription == null) {
-      console.error('web push not subscribed')
-      return
+      console.error("web push not subscribed");
+      return;
     }
 
-    await fetch('/api/notification', {
-      method: 'POST',
+    await fetch("/api/notification", {
+      method: "POST",
       headers: {
-        'Content-type': 'application/json'
+        "Content-type": "application/json",
       },
       body: JSON.stringify({
-        subscription
-      })
-    })
-  }
+        subscription,
+      }),
+    });
+  };
 
   return (
     <>
       <Head>
         <title>Best Pwa mix</title>
       </Head>
-      <h1>Next.js + PWA = AWESOME!</h1>
-      <button onClick={subscribeButtonOnClick} disabled={isSubscribed}>
-        Subscribe
-      </button>
-      <button onClick={unsubscribeButtonOnClick} disabled={!isSubscribed}>
-        Unsubscribe
-      </button>
-      <button onClick={sendNotificationButtonOnClick} disabled={!isSubscribed}>
-        Send Notification
-      </button>
+      <Layout>
+        <h1>Next.js + PWA = AWESOME!</h1>
+        <button onClick={subscribeButtonOnClick} disabled={isSubscribed}>
+          Subscribe
+        </button>
+        <button onClick={unsubscribeButtonOnClick} disabled={!isSubscribed}>
+          Unsubscribe
+        </button>
+        <button
+          onClick={sendNotificationButtonOnClick}
+          disabled={!isSubscribed}
+        >
+          Send Notification
+        </button>
+
+        <h1>Magic Example</h1>
+
+        <p>Steps to test this authentication example:</p>
+
+        <ol>
+          <li>Click Login and enter an email.</li>
+          <li>
+            You'll be redirected to Home. Click on Profile, notice how your
+            session is being used through a token stored in a cookie.
+          </li>
+          <li>
+            Click Logout and try to go to Profile again. You'll get redirected
+            to Login.
+          </li>
+        </ol>
+
+        <p>
+          To learn more about Magic, visit their{" "}
+          <a
+            href="https://docs.magic.link/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            documentation
+          </a>
+          .
+        </p>
+
+        {user && (
+          <>
+            <p>Currently logged in as:</p>
+            <pre>{JSON.stringify(user, null, 2)}</pre>
+          </>
+        )}
+
+        <style jsx>{`
+          li {
+            margin-bottom: 0.5rem;
+          }
+          pre {
+            white-space: pre-wrap;
+            word-wrap: break-word;
+          }
+        `}</style>
+      </Layout>
     </>
-  )
-}
+  );
+};
